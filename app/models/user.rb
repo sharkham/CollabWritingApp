2c82fbe927e2ec5214599498e142631f5eb8b2b5
Sam Markham <sam.e.markham@gmail.com>
2019-12-03 16:32:08 -0800

    Adds has_secure_password to User model and enables bcrypt in Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :memberships
   has_many :novels, through: :memberships
+  has_secure_password
 end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_12_04_002027) do
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "novel_id", null: false
+    t.integer "user_id", null: false
+    t.string "role"
+    t.index ["novel_id"], name: "index_memberships_on_novel_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "novels", force: :cascade do |t|
+    t.string "title"
+    t.text "summary"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "snippets", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "summary"
+    t.string "type"
+    t.integer "novel_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["novel_id"], name: "index_snippets_on_novel_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "memberships", "novels"
+  add_foreign_key "memberships", "users"
+  add_foreign_key "snippets", "novels"
+end
