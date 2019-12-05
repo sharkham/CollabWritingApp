74f498f5cb6b28551969e2d6f4385e082431443b
Sam Markham <sam.e.markham@gmail.com>
2019-12-05 14:58:31 -0800

    Adds form for new snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,17 @@
 class SnippetsController < ApplicationController
+  def new
+    @snippet = Novel.find_by(id: params[:novel_id]).snippets.build
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
