0c345d66d495b9493be4721bcaaf06ec64b6a6b9
Sam Markham <sam.e.markham@gmail.com>
2019-12-04 21:05:30 -0800

    Builds create action in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Incorrect login info, please try again."
+      redirect_to login_path
+    end
   end
 
   def destroy
