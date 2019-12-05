78d6bee0a3f5db9e2f3b66c95da3257a57d5ef95
Sam Markham <sam.e.markham@gmail.com>
2019-12-05 11:50:54 -0800

    Specifies which novel routes should be nested under users

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
 
   delete '/logout' => "sessions#destroy"
 
-  resources :novels do
+  resources :novels, only: [:show, :edit, :update, :destroy] do
     resources :memberships
     resources :snippets
   end
-  resources :users
+  resources :users do
+    resources :novels, only: [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
