1546be153f84bdd9c4940523aef3563f30f67f12
Sam Markham <sam.e.markham@gmail.com>
2019-12-05 15:00:48 -0800

    Adds a create action for snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -4,6 +4,12 @@ class SnippetsController < ApplicationController
   end
 
   def create
+    @snippet = Novel.find_by(id: params[:novel_id]).snippets.build
+    if @snippet.save
+      redirect_to novel_path(@snippet.novel)
+    else
+      render :new
+    end
   end
 
   def edit
