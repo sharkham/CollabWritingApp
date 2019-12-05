f36ec8e4747c2420f48475c8a74aa14f4cb71f98
Sam Markham <sam.e.markham@gmail.com>
2019-12-05 10:34:13 -0800

    Adds helper methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!session[:user_id]
+    # change this so it's checking for current_user instead
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to root_path if !logged_in?
   end
 end
