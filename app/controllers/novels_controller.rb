0265123235f4c86fec7e04c839a615448aab78a9
Sam Markham <sam.e.markham@gmail.com>
2019-12-05 11:29:10 -0800

    Adds basic novel_params method and novel create method to NovelsController

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -2,9 +2,19 @@ class NovelsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
   def new
+    @novel = Novel.new
+  end
+
+  def create
   end
 
   def index
+  end
+
+  private
+
+  def novel_params
+    params.require(:novel).permit(:title, :summary)
   end
 
 end
