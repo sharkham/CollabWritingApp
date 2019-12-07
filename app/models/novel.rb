615316473c47a0cbf474fa47f6b9568d9edc7670
Sam Markham <sam.e.markham@gmail.com>
2019-12-06 16:45:13 -0800

    Adds styling to login page errors

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -6,18 +6,6 @@ class Novel < ApplicationRecord
 
   accepts_nested_attributes_for :memberships
 
-  # def membership_attributes=(membership_attributes)
-  #   membership_attributes.values.each do |mem_attr|
-  #     # what Avi did here:
-  #     # if membership_attributes[:name].present?
-  #     unless mem_attr["role"] == ""
-  #       membership = membership.create_by(mem_attr)
-  #       # if !self.categories.include?(membership)
-  #         self.memberships << membership
-  #       #   # what Avi did for this instead: (building the association directly)
-  #       #   # self.post_categories.build(:category => category)
-  #       # end
-  #     end
-  #   end
-  # end
+
+
 end
