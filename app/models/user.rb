c86edb5cf45cf093578754161086efc0bd96d174
Sam Markham <sam.e.markham@gmail.com>
2019-12-07 10:45:23 -0800

    Adds a method to user model trying to sort users by if they already have a membership in a novel or not (commented out).

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ApplicationRecord
   validates :username, :email, uniqueness: { case_sensitive: false }
   validates :password, length: { in: 5..20, wrong_length: "Password must be between 5 and 20 characters." }
 
+  # def self.not_members(novel)
+  #   # self.where()
+  #   #looking for instances do not have a membership that contains this novel id.
+  #   self.all.each do |user|
+  #     array = []
+  #     if !user.novels
+  #       array << user
+  #     elsif user.novels && !user.novels.include?(novel)
+  #       array << user
+  #     end
+  #     # if user.novels && !user.novels.include?(novel)
+  #     #   array << user
+  #     # end
+  #     array
+  #   end
+  # end
+
 end
