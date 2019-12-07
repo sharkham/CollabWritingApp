f7f94ca25a0427051508ff6cd11dabb8aa354ca6
Sam Markham <sam.e.markham@gmail.com>
2019-12-06 16:33:47 -0800

    Adds validations to user model and styling for field_with_errors in stylesheet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :novels, through: :memberships
   has_secure_password
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 5..20, wrong_length: "Password must be between 5 and 20 characters." }
+
 end
