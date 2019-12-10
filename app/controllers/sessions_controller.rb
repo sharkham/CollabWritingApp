6d00d466af2d3057fabed8c3f8e0d6cb5e9e0fb3
Sam Markham <sam.e.markham@gmail.com>
2019-12-10 12:43:28 -0800

    Changes syntax for error handling for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      flash[:message] = "Incorrect login info, please try again."
+      flash[:incorrect_login] = "Incorrect login info, please try again."
       redirect_to login_path
     end
   end
