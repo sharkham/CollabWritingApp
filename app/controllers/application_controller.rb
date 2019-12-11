439d8d7d382db48e296142f5cd6715f31ba3eaf7
Sam Markham <sam.e.markham@gmail.com>
2019-12-11 11:38:15 -0800

    Hides edit novel link unless user is an admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin_of?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
