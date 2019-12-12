dc2b6239c080eb0030bbe6cd16648a584db57125
Sam Markham <sam.e.markham@gmail.com>
2019-12-12 14:33:34 -0800

    Comments out edit and update actions in users controller because they're not going to work that way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,30 +20,30 @@ class UsersController < ApplicationController
     redirect_to root_path if !@user
   end
 
-  def edit
-    require_login
-    @user = User.find_by(id: params[:id])
-    if @user != current_user || !@user
-      flash[:message] = "You can only edit your own profile."
-      redirect_to user_path(current_user)
-    end
-  end
+  # def edit
+  #   require_login
+  #   @user = User.find_by(id: params[:id])
+  #   if @user != current_user || !@user
+  #     flash[:message] = "You can only edit your own profile."
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
 
-  def update
-    # binding.pry
-    @user = User.find_by(id: params[:id])
-    @user.bio = params[:user][:bio]
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :edit
-    end
-    # if @user.update_attributes(bio_param)
-    #   redirect_to user_path(@user)
-    # else
-    #   render :edit
-    # end
-  end
+  # def update
+  #   # binding.pry
+  #   @user = User.find_by(id: params[:id])
+  #   @user.bio = params[:user][:bio]
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :edit
+  #   end
+  #   # if @user.update_attributes(bio_param)
+  #   #   redirect_to user_path(@user)
+  #   # else
+  #   #   render :edit
+  #   # end
+  # end
 
 
   private
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
-  def bio_param
-    params.require(:user).permit(:bio)
-  end
+  # def bio_param
+  #   params.require(:user).permit(:bio)
+  # end
 end
