2e04f2a21ca4a89224bdadc0359ef83594b3b4dd
Sam Markham <sam.e.markham@gmail.com>
2019-12-12 13:44:24 -0800

    Adds method to snippet model to find snippets with a certain tag in a certain novel

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,4 +2,8 @@ class Snippet < ApplicationRecord
   belongs_to :novel
 
   validates :title, presence: true
+
+  def self.grab_tag(tag, novel_id)
+    self.where(tag: tag, novel_id: novel_id)
+  end
 end
