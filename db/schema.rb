24e43cfc158bc873bf88c388ac146987303bc0fb
Sam Markham <sam.e.markham@gmail.com>
2019-12-12 11:53:16 -0800

    Adds bio attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_002027) do
+ActiveRecord::Schema.define(version: 2019_12_12_195216) do
 
   create_table "memberships", force: :cascade do |t|
     t.integer "novel_id", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_002027) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "bio"
   end
 
   add_foreign_key "memberships", "novels"
