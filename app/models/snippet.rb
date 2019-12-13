470cf6b084067780f1f1667035aaae2bc440f894
Sam Markham <sam.e.markham@gmail.com>
2019-12-12 17:31:50 -0800

    Adds has_rich_text to snippet model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,6 +2,7 @@ class Snippet < ApplicationRecord
   belongs_to :novel
 
   validates :title, presence: true
+  has_rich_text :content
 
   def self.grab_tag(tag, novel_id)
     self.where(tag: tag, novel_id: novel_id)
